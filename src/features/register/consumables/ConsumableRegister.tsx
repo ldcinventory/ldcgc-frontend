75077e1c379944f67aad01f562d79e4a90c94681
srello <srello0410@gmail.com>
2024-04-01 20:38:01 +0200

    mobile table done

--- src/features/register/consumables/ConsumableRegister.tsx
+++ src/features/register/consumables/ConsumableRegister.tsx
@@ -3,7 +3,7 @@ import { ConsumablesRegisterTable } from "./components/ConsumablesRegisterTable"
 
 export function ConsumablesRegister() {
   return (
-    <div className="flex flex-col items-start mt-10 mx-20">
+    <div className="flex flex-col items-start mt-10 mx-5 lg:mx-20">
       <AddConsumableRegisterModal />
       <ConsumablesRegisterTable />
     </div>
